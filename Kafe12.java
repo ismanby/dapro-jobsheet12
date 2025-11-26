f2ee5b74a1e91cda81031a3d51655c1df2fe3602
ismanby <ismaanby@gmail.com>
2025-11-26 19:37:43 +0700

    Percobaan 3

--- Kafe12.java
+++ Kafe12.java
@@ -1,23 +1,25 @@
+import java.util.Scanner;
+
 public class Kafe12 {
     public static void main(String[] args) {
-        Menu("Andi", true, "DISKON30");
+        Scanner sc = new Scanner (System.in);
+
+        Menu();
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp " + totalHarga);
+
+        sc.close();
     }
     
-    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
-        System.out.println("Selamat datang, " + namaPelanggan + "!");
-
-        if (isMember) {
-            System.out.println("Anda adalah member dapatkan diskon 10% untuk setip pembelian!");
-        }
-
-        if(kodePromo == "DISKON50") {
-            System.out.println("Selamat! Anda mendapatkan diskon sebesar 50%");
-        } else if(kodePromo == "DISKON30") {
-            System.out.println("Selamat! Anda mendapatkan diskon sebesar 30%");
-        } else {
-            System.out.println("Kode Invalid!");
-        }
-
+    public static void Menu() {
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -27,5 +29,12 @@ public class Kafe12 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("============================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
+        return hargaTotal;
     }
 }
