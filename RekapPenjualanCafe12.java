7acaaad76c538aa1a264300a16095dfc97fd3c7e
ismanby <ismaanby@gmail.com>
2025-11-27 10:28:31 +0700

    Tugas 3B - Rekap Penjualan

--- RekapPenjualanCafe12.java
+++ RekapPenjualanCafe12.java
@@ -3,11 +3,28 @@ import java.util.Scanner;
 public class RekapPenjualanCafe12 {
     static Scanner sc = new Scanner(System.in);
 
-    static String[] menu = { "Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan" };
+    static String[] menu;
 
-    static int[][] penjualan = new int[5][7];
+    static int[][] penjualan;
 
     public static void main(String[] args) {
+
+        System.out.print("Masukkan jumlah menu: ");
+        int jumlahMenu = sc.nextInt();
+
+        System.out.print("Masukkan jumlah hari: ");
+        int jumlahHari = sc.nextInt();
+        sc.nextLine();
+
+        menu = new String[jumlahMenu];
+        penjualan = new int[jumlahMenu][jumlahHari];
+
+        System.out.println("\n=== INPUT NAMA MENU ===");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Nama menu ke-" + (i + 1) + ": ");
+            menu[i] = sc.nextLine();
+        }
+
         inputData();
         tampilData();
         menuTertinggi();
@@ -17,7 +34,7 @@ public class RekapPenjualanCafe12 {
     static void inputData() {
         System.out.println("=== INPUT DATA PENJUALAN ===");
         for (int i = 0; i < penjualan.length; i++) {
-            System.out.println("Masukkan data untuk menu: " + menu[i]);
+            System.out.println("Masukkan data untuk menu " + menu[i]);
             for (int j = 0; j < penjualan[i].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + " : ");
                 penjualan[i][j] = sc.nextInt();
@@ -28,8 +45,13 @@ public class RekapPenjualanCafe12 {
 
     static void tampilData() {
         System.out.println("\n=== DATA PENJUALAN ===");
-        System.out.printf("%-15s %-10s %-10s %-11s %-12s %-13s %-11s %-8s\n", "Menu", "Hari 1", "Hari 2", "Hari 3", "Hari 4", "Hari 5", "Hari 6", "Hari 7");
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.printf("%-18s", "Menu");
+
+        for (int j = 0; j < penjualan[0].length; j++) {
+            System.out.printf("%-12s", "Hari " + (j + 1));
+        }
+        System.out.println();
+        System.out.println("----------------------------------------------------------------------------------------");
 
         for (int i = 0; i < penjualan.length; i++) {
             System.out.printf("%-18s", menu[i]);
