1300025bb322525d4cdc6128975919231eafdf4a
ismanby <ismaanby@gmail.com>
2025-11-27 10:34:07 +0700

    Modifikasi Percobaan 3 - Pertanyaan 4

--- Kafe12.java
+++ Kafe12.java
@@ -2,23 +2,46 @@ import java.util.Scanner;
 
 public class Kafe12 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        Menu();
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        String[] namaMenu = {"Kopi Hitam", "Cappuccino", "Latte", "Teh Tarik", "Roti Bakar", "Mie Goreng"};
 
-        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
-        int pilihanMenu = sc.nextInt();
+        double totalKeseluruhan = 0;
+        char lanjut = 0;
 
-        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
-        int banyakItem = sc.nextInt();
+        do {
+            Menu();
 
-        double totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, "");
+            System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+            int pilihanMenu = sc.nextInt();
 
-        System.out.println("Total harga untuk pesanan Anda: Rp " + totalHarga);
+            if (pilihanMenu < 1 || pilihanMenu > 6) {
+                System.out.println("Menu tidak tersedia!");
+                continue;
+            }
+
+            System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+            int banyakItem = sc.nextInt();
+
+            double totalHarga = hargaItems[pilihanMenu - 1] * banyakItem;
+            totalKeseluruhan += totalHarga;
+
+            System.out.println("Pesanan: " + namaMenu[pilihanMenu - 1] + " x " + banyakItem);
+            System.out.println("Subtotal: Rp " + totalHarga);
+
+            System.out.print("\nApakah Anda ingin memesan menu lain? (y/n) : ");
+            lanjut = sc.next().charAt(0);
+
+        } while (lanjut == 'y' || lanjut == 'Y');
+
+        System.out.println("\n====================================");
+        System.out.println("TOTAL KESELURUHAN PESANAN: Rp " + totalKeseluruhan);
+        System.out.println("====================================");
 
         sc.close();
     }
-    
+
     public static void Menu() {
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
@@ -28,27 +51,5 @@ public class Kafe12 {
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("============================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
-    }
-
-    public static double hitungTotalHarga(int pilihanMenu, int banyakItem, String kodePromo) {
-        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
-
-        double hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
-        double diskon; 
-
-        if (kodePromo == "DISKON30" ) {
-            diskon = (double) hargaTotal * 0.3;
-            System.out.println("Anda mendapatkan diskon sebesar Rp " + diskon);
-            hargaTotal = hargaTotal - diskon;
-        } else if (kodePromo == "DISKON50") {
-            diskon = (double) hargaTotal * 0.3;
-            System.out.println("Anda mendapatkan diskon sebesar Rp " + diskon);
-            hargaTotal = hargaTotal - diskon;
-        } else {
-            System.out.println("Kode Invalid!");
-        }
-
-        return hargaTotal;
     }
 }
